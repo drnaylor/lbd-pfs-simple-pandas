4faa5eb1cb205a12f38c9c8afa777797be32e73f
Daniel Naylor <93977310+drnaylor@users.noreply.github.com>
2026-03-17 16:22:34 +0000

    Arrange logic in functions for easier testing

--- calculate.py
+++ calculate.py
@@ -5,43 +5,51 @@ Assumes that `postcode.parquet` and `fuel.parquet` exist.
 """
 
 import argparse
-from typing import cast
+from typing import cast, NamedTuple
 
 import pandas as pd
 from haversine import haversine, Unit
 
-parser = argparse.ArgumentParser()
-parser.add_argument("postcode", type=str)
-parser.add_argument("fuel_type", type=str)
-parser.add_argument("mpg", type=float)
-parser.add_argument("litres", type=float)
 
-args = parser.parse_args()
+Arguments = NamedTuple("Arguments", [("postcode", str), ("fuel_type", str), ("mpg", float), ("litres", float)])
 
-# get the data files into memory
-fuel_prices = pd.read_parquet("fuel.parquet")
-postcodes = pd.read_parquet("postcode.parquet")
+def parse_args() -> Arguments:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("postcode", type=str)
+    parser.add_argument("fuel_type", type=str)
+    parser.add_argument("mpg", type=float)
+    parser.add_argument("litres", type=float)
 
-fuel_type: str = args.fuel_type.strip().upper()
-fuel_types = ["E5", "E10", "B7S", "B7P", "B10", "HVO"]
-if fuel_type not in fuel_types:
-    raise ValueError(f"Fuel type must be one of {", ".join(fuel_types)}, but {fuel_type} was specified")
+    args: argparse.Namespace = parser.parse_args()
 
-fuel_prices = fuel_prices[fuel_prices[fuel_type].notna()]
+    fuel_type: str = args.fuel_type.strip().upper()
+    fuel_types = ["E5", "E10", "B7S", "B7P", "B10", "HVO"]
+    if fuel_type not in fuel_types:
+        raise ValueError(f"Fuel type must be one of {", ".join(fuel_types)}, but {fuel_type} was specified")
+    
+    # We check that the postcode is valid, but we also ensure that we put a space in if necessary
+    postcode_arg: str = args.postcode.strip().upper()
+    if " " not in postcode_arg:
+        postcode_arg = " ".join([postcode_arg[:-3], postcode_arg[-3:]])
 
-# We check that the postcode is valid, but we also ensure that we put a space in if necessary
-postcode_arg: str = args.postcode.strip().upper()
-if " " not in postcode_arg:
-    postcode_arg = " ".join([postcode_arg[:-3], postcode_arg[-3:]])
+    return Arguments(postcode_arg, fuel_type, args.mpg, args.litres)
 
-# We need to reset the index as it isn't "row number", it's intended to be a static identifier
-# However, we can't easily mix "at" and "iat" later (for getting single values out), so it's easier to just do this.
-postcode_row: pd.DataFrame = postcodes[postcodes["postcode"] == postcode_arg][["latitude", "longitude"]].head(1).reset_index()
-if postcode_row.empty:
-    raise ValueError(f"Postcode {postcode_arg} does not exist")
+def read_files() -> tuple[pd.DataFrame, pd.DataFrame]:
+    # get the data files into memory
+    return (pd.read_parquet("fuel.parquet"), pd.read_parquet("postcode.parquet"))
 
-# The following are floats.
-home_latlong: tuple[float, float] = cast(tuple[float, float], (postcode_row.at[0, "latitude"], postcode_row.at[0, "longitude"]))
+def filter_fuel_types(fuel_prices: pd.DataFrame, fuel_type: str) -> pd.DataFrame:
+    return fuel_prices[fuel_prices[fuel_type].notna()]
+
+def get_lat_long(postcode_df: pd.DataFrame, postcode: str) -> tuple[float, float]:
+    # We need to reset the index as it isn't "row number", it's intended to be a static identifier
+    # However, we can't easily mix "at" and "iat" later (for getting single values out), so it's easier to just do this.
+    postcode_row: pd.DataFrame = postcode_df[postcode_df["postcode"] == postcode][["latitude", "longitude"]].head(1).reset_index()
+    if postcode_row.empty:
+        raise ValueError(f"Postcode {postcode} does not exist")
+
+    # The following are floats.
+    return cast(tuple[float, float], (postcode_row.at[0, "latitude"], postcode_row.at[0, "longitude"]))
 
 def distance_in_km(latlong: pd.Series, home_latlong: tuple[float, float]) -> float:
     """
@@ -55,24 +63,38 @@ def distance_in_km(latlong: pd.Series, home_latlong: tuple[float, float]) -> flo
     """
     return haversine(latlong, home_latlong, unit=Unit.KILOMETERS)
 
-fuel_prices["distance"] = fuel_prices[["latitude", "longitude"]].apply(distance_in_km, axis=1, home_latlong=home_latlong)
-fuel_prices["total_fuel_cost"] = fuel_prices[fuel_type] * float(args.litres)
-fuel_prices["total_cost_of_driving"] = 282.5 * fuel_prices["distance"] / float(args.mpg) # cost in pence
-fuel_prices["full_cost"] = fuel_prices["total_fuel_cost"] + fuel_prices["total_cost_of_driving"]
 
-display_frame = fuel_prices[["brand_name", "trading_name", "postcode", fuel_type, "distance", "total_fuel_cost", "total_cost_of_driving", "full_cost"]].sort_values(by="full_cost", ascending=True).rename(
-    columns={
-        "brand_name": "Brand Name",
-        "trading_name": "Trading Name",
-        "postcode": "Postcode",
-        "distance": "Distance to forecourt (km)",
-        fuel_type: "Price per litre (pence)",
-        "total_fuel_cost": "Total cost of fuel (pence)",
-        "total_cost_of_driving": "Straight line driving cost (pence)",
-        "full_cost": "Total cost (pence)"
-    }
-)
+def add_distance_based_columns(fuel_prices: pd.DataFrame, args: Arguments, home_latlong: tuple[float, float]) -> pd.DataFrame:
+    fuel_prices["distance"] = fuel_prices[["latitude", "longitude"]].apply(distance_in_km, axis=1, home_latlong=home_latlong)
+    fuel_prices["total_fuel_cost"] = fuel_prices[args.fuel_type] * float(args.litres)
+    fuel_prices["total_cost_of_driving"] = 282.5 * fuel_prices["distance"] / float(args.mpg) # cost in pence
+    fuel_prices["full_cost"] = fuel_prices["total_fuel_cost"] + fuel_prices["total_cost_of_driving"]
+    return fuel_prices
 
-print("Cheapest 10 forecourts")
-print("-----")
-print(display_frame.head(10).to_string(index=False))
+
+def display(fuel_prices: pd.DataFrame, fuel_type: str) -> None:
+    display_frame = fuel_prices[["brand_name", "trading_name", "postcode", fuel_type, "distance", "total_fuel_cost", "total_cost_of_driving", "full_cost"]].sort_values(by="full_cost", ascending=True).rename(
+        columns={
+            "brand_name": "Brand Name",
+            "trading_name": "Trading Name",
+            "postcode": "Postcode",
+            "distance": "Distance to forecourt (km)",
+            fuel_type: "Price per litre (pence)",
+            "total_fuel_cost": "Total cost of fuel (pence)",
+            "total_cost_of_driving": "Straight line driving cost (pence)",
+            "full_cost": "Total cost (pence)"
+        }
+    )
+
+    print("Cheapest 10 forecourts")
+    print("-----")
+    print(display_frame.head(10).to_string(index=False))
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    fuel_df, postcode_df = read_files()
+    fuel_df = filter_fuel_types(fuel_df, args.fuel_type)
+    home_latlong = get_lat_long(postcode_df, args.postcode)
+    fuel_df = add_distance_based_columns(fuel_df, args, home_latlong)
+    display(fuel_df, args.fuel_type)
--- prepare.py
+++ prepare.py
@@ -10,33 +10,43 @@ from typing import Optional, Any
 import pandas as pd
 import numpy as np
 
-parser = argparse.ArgumentParser("PFS Data Preparation")
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser("PFS Data Preparation")
 
-parser.add_argument("fuel_data", type=str, help="The fuel CSV file to ingest")
-parser.add_argument("postcode_data", type=str, help="The postcode CSV file to ingest")
+    parser.add_argument("fuel_data", type=str, help="The fuel CSV file to ingest")
+    parser.add_argument("postcode_data", type=str, help="The postcode CSV file to ingest")
 
-args = parser.parse_args()
+    return parser.parse_args()
 
-fuel_dataframe: pd.DataFrame = pd.read_csv(args.fuel_data, header=0)
-postcode_dataframe: pd.DataFrame = pd.read_csv(args.postcode_data, header=0)
 
-# Clean up for the fuel dataframe
+def parse_dataframes(args: argparse.Namespace) -> tuple[pd.DataFrame, pd.DataFrame]:
+    fuel_dataframe: pd.DataFrame = pd.read_csv(args.fuel_data, header=0)
+    postcode_dataframe: pd.DataFrame = pd.read_csv(args.postcode_data, header=0)
+    return (fuel_dataframe, postcode_dataframe)
 
-# We start with conditions where QA/Test entries are in there. Using logical
-# ORs (basically from numpy), we create a series of True/False entries, which
-# we then use to filter out these results.
-#
-# Note that we return `True` if the result IS a QA entry, so we'll negate that
-# to filter them out later on.
-qa_results = (
-    fuel_dataframe["forecourts.trading_name"].str.contains("preprod", case=False) | 
-    fuel_dataframe["forecourts.trading_name"].str.contains("-new", case=False) |
-    fuel_dataframe["forecourts.brand_name"].isnull() |
-    fuel_dataframe["forecourts.brand_name"].str.contains("pre-prod", case=False)
-)
 
-# This contains the dataframe without QA entries
-fuel_dataframe = fuel_dataframe[~qa_results]
+def remove_qa_entries(fuel_dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean up for the fuel dataframe
+
+    We start with conditions where QA/Test entries are in there. Using logical
+    ORs (basically from numpy), we create a series of True/False entries, which
+    we then use to filter out these results.
+    
+    Note that we return `True` if the result IS a QA entry, so we'll negate that
+    to filter them out later on.
+    """
+    
+    qa_results = (
+        fuel_dataframe["forecourts.trading_name"].str.contains("preprod", case=False) | 
+        fuel_dataframe["forecourts.trading_name"].str.contains("-new", case=False) |
+        fuel_dataframe["forecourts.brand_name"].isnull() |
+        fuel_dataframe["forecourts.brand_name"].str.contains("pre-prod", case=False)
+    )
+
+    # This contains the dataframe without QA entries
+    return fuel_dataframe[~qa_results] # type: ignore[reportAssignmentType]
+
 
 def parse_datetime(input: Any) -> Optional[datetime]:
     try:
@@ -76,50 +86,68 @@ def clean_prices(dataframe: pd.DataFrame, fuel_type: str) -> pd.Series:
         ]
     )
 
+def clean_fuel_prices(fuel_dataframe: pd.DataFrame) -> pd.DataFrame:
+    fuel_dataframe["E5"] = clean_prices(fuel_dataframe, "E5")
+    fuel_dataframe["E10"] = clean_prices(fuel_dataframe, "E10")
+    fuel_dataframe["B7S"] = clean_prices(fuel_dataframe, "B7S")
+    fuel_dataframe["B7P"] = clean_prices(fuel_dataframe, "B7P")
+    fuel_dataframe["B10"] = clean_prices(fuel_dataframe, "B10")
+    fuel_dataframe["HVO"] = clean_prices(fuel_dataframe, "HVO")
+    return fuel_dataframe
 
-fuel_dataframe["E5"] = clean_prices(fuel_dataframe, "E5")
-fuel_dataframe["E10"] = clean_prices(fuel_dataframe, "E10")
-fuel_dataframe["B7S"] = clean_prices(fuel_dataframe, "B7S")
-fuel_dataframe["B7P"] = clean_prices(fuel_dataframe, "B7P")
-fuel_dataframe["B10"] = clean_prices(fuel_dataframe, "B10")
-fuel_dataframe["HVO"] = clean_prices(fuel_dataframe, "HVO")
 
-# now, we need to link this to the postcode data
-# for this, I've used data from https://www.freemaptools.com/download-uk-postcode-lat-lng.htm
-# but before that, the postcodes in the petrol CSV files aren't all that good either, so we do a quick patch up
+def clean_fuel_postcodes(fuel_dataframe: pd.DataFrame) -> pd.DataFrame:
+    # now, we need to link this to the postcode data
+    # for this, I've used data from https://www.freemaptools.com/download-uk-postcode-lat-lng.htm
+    # but before that, the postcodes in the petrol CSV files aren't all that good either, so we do a quick patch up
 
-pfs_postcode_series = fuel_dataframe["forecourts.location.postcode"].str.strip() 
-fuel_dataframe["postcode"] = pfs_postcode_series.case_when(
-    [
-        # if no space is present, add a space. Otherwise, leave it as is
-        (
-            ~pfs_postcode_series.str.contains(" "), 
-            pfs_postcode_series.str.slice(stop=-3).str.cat(pfs_postcode_series.str.slice(start=-3), sep=" ")
-        )
-    ]
-)
+    pfs_postcode_series = fuel_dataframe["forecourts.location.postcode"].str.strip() 
+    fuel_dataframe["postcode"] = pfs_postcode_series.case_when(
+        [
+            # if no space is present, add a space. Otherwise, leave it as is
+            (
+                ~pfs_postcode_series.str.contains(" "), 
+                pfs_postcode_series.str.slice(stop=-3).str.cat(pfs_postcode_series.str.slice(start=-3), sep=" ")
+            )
+        ]
+    )
 
-# Before we merge with the postcode dataframe, we're going to only take the columns we want out of this
-fuel_dataframe = fuel_dataframe.rename(columns={
-    "forecourts.trading_name": "trading_name",
-    "forecourts.brand_name": "brand_name"
-})[["brand_name", "trading_name", "postcode", "E5", "E10", "B7S", "B7P", "B10", "HVO"]]
+    return fuel_dataframe
 
-# Merge on the postcode column, which gives us latitude, and drop the ID column from the right
-final_dataframe = pd.merge(
-    fuel_dataframe, 
-    postcode_dataframe,
-    how="inner",
-    on="postcode"
-)[['brand_name', 'trading_name', 'postcode', 'E5', 'E10', 'B7S', 'B7P', 'B10', 'HVO', 'latitude', 'longitude']]
 
-with open("fuel.parquet", mode="wb") as fuel_file:
-    # We do it this way to ensure the file can be overwritten
-    fuel_file.write(final_dataframe.to_parquet(path=None, index=False))
+def clean_fuel_lat_long(fuel_dataframe: pd.DataFrame, postcode_dataframe: pd.DataFrame) -> pd.DataFrame:
+    # Before we merge with the postcode dataframe, we're going to only take the columns we want out of this
+    fuel_dataframe = fuel_dataframe.rename(columns={
+        "forecourts.trading_name": "trading_name",
+        "forecourts.brand_name": "brand_name"
+    })[["brand_name", "trading_name", "postcode", "E5", "E10", "B7S", "B7P", "B10", "HVO"]] # type: ignore[reportAssignmentType]
 
-with open("postcode.parquet", mode="wb") as postcode_file:
-    # axis 1 is columns, axis 0 is rows (and the default) -- drop is usually used to drop
-    # specific rows by index
-    postcode_file.write(postcode_dataframe.drop("id", axis=1).to_parquet(path=None, index=False))
+    # Merge on the postcode column, which gives us latitude, and drop the ID column from the right
+    return pd.merge(
+        fuel_dataframe, 
+        postcode_dataframe,
+        how="inner",
+        on="postcode"
+    )[['brand_name', 'trading_name', 'postcode', 'E5', 'E10', 'B7S', 'B7P', 'B10', 'HVO', 'latitude', 'longitude']] # type: ignore[reportAssignmentType]
 
-print("Data preparation complete.")
+def write_files(fuel_dataframe: pd.DataFrame, postcode_dataframe: pd.DataFrame):
+    with open("fuel.parquet", mode="wb") as fuel_file:
+        # We do it this way to ensure the file can be overwritten
+        fuel_file.write(fuel_dataframe.to_parquet(path=None, index=False))
+
+    with open("postcode.parquet", mode="wb") as postcode_file:
+        # axis 1 is columns, axis 0 is rows (and the default) -- drop is usually used to drop
+        # specific rows by index
+        postcode_file.write(postcode_dataframe.drop("id", axis=1).to_parquet(path=None, index=False))
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    fuel_dataframe, postcode_dataframe = parse_dataframes(args)
+    fuel_dataframe = remove_qa_entries(fuel_dataframe)
+    fuel_dataframe = clean_fuel_postcodes(fuel_dataframe)
+    fuel_dataframe = clean_fuel_prices(fuel_dataframe)
+    fuel_dataframe = clean_fuel_lat_long(fuel_dataframe, postcode_dataframe)
+    write_files(fuel_dataframe, postcode_dataframe)
+
+    print("Data preparation complete.")
